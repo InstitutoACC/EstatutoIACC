698cfb7ecf706894f878f7fad56924c54700b56a
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-03-13 08:35:38 -0300

    📖 First estatuto commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Estatuto Social del Instituto Atlético Central Córdoba, publicado el 22/08/15.
+Generado con la librería `bookdown` de R.
---
+++ utils.R
@@ -0,0 +1,8 @@
+get_art_meta <- function(art_num) {
+  paste0(
+    '<div id="art',
+    art_num,
+    '">'
+    # <a class="twitter-share-button" href="https://twitter.com/intent/tweet">aa</a>
+  )
+}
