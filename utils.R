83d5c173f9b891d61bb3356a997628a380337bd8
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-03-13 09:57:09 -0300

    📖 Adding tweet feature

--- utils.R
+++ utils.R
@@ -2,7 +2,12 @@ get_art_meta <- function(art_num) {
   paste0(
     '<div id="art',
     art_num,
-    '">'
-    # <a class="twitter-share-button" href="https://twitter.com/intent/tweet">aa</a>
+    '">',
+    '<a class="btn pull-right js-toolbar-action" aria-label="" href=',
+    '"https://twitter.com/intent/tweet?text=Mira+el+art%C3%ADculo+',
+    art_num, '%C2%BA+del+%23EstatutoIACC%3A+',
+    'https://InstitutoACC.github.io/EstatutoIACC/del-nombre-domicilio-fines-distintivos-y-patrimonio-social.html%23art',
+    art_num,
+    '"><i class="fa fa-twitter"></i></a>\n'
   )
 }
