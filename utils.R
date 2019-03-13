5338641ca70f1577c58689461faacd3d71e5995a
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-03-13 11:10:55 -0300

    📖 Fixing tweet

--- utils.R
+++ utils.R
@@ -1,4 +1,4 @@
-get_art_meta <- function(art_num) {
+get_art_meta <- function(cap_num, art_num) {
   paste0(
     '<div id="art',
     art_num,
@@ -6,7 +6,9 @@ get_art_meta <- function(art_num) {
     '<a class="btn pull-right js-toolbar-action" aria-label="" target="blank" href=',
     '"https://twitter.com/intent/tweet?text=Mira+el+art%C3%ADculo+',
     art_num, '%C2%BA+del+%23EstatutoIACC+%3A+',
-    'https://InstitutoACC.github.io/EstatutoIACC/del-nombre-domicilio-fines-distintivos-y-patrimonio-social.html%23art',
+    'https://InstitutoACC.github.io/EstatutoIACC/cap',
+    cap_num,
+    '.html%23art',
     art_num,
     '"><i class="fa fa-twitter"></i></a>\n'
   )
